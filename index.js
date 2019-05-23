325b94e76558d21edff783c5ad8274cdb2008a29
Azul <diego.kfu@hotmail.com>
2019-05-23 16:22:41 -0300

    Added steamid to isSteamID64

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const moment = require('moment');
 const numeral = require('numeral');
 const fs = require('graceful-fs');
+const SteamID = require('steamid');
 
 const SteamID64Regex = /[0-9]{17}/;
 const TradeOfferURLRegex = /https?:\/\/steamcommunity\.com\/tradeoffer\/new\/\?partner=[0-9]*&token=[a-zA-Z0-9_-]*/;
@@ -30,8 +31,13 @@ async function isTradeOfferURL(str) {
     return TradeOfferURLRegex.test(str);
 }
 
-async function isSteamID64(str) {
-    return SteamID64Regex.test(str);
+async function isSteamID64(str) {    
+    try {
+        const sid = new SteamID(str);
+        return SteamID64Regex.test(sid.getSteamID64());
+    } catch(e){
+        return false;
+    }
 }
 
 async function GetSteamID64FromURL(str) {
