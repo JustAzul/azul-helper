c464bf1a236e9a9ddc2477a8c5e476bbd3a7ec89
Azul <diego.kfu@hotmail.com>
2021-02-11 20:01:12 -0300

    Added Worker on readJSON

---
+++ components/WorkerReadJson.js
@@ -0,0 +1,33 @@
+const { Worker, isMainThread, workerData, parentPort } = require("worker_threads");
+const Read = require('./ReadJson');
+
+if(isMainThread) {
+    module.exports = function(Filename = "") {
+        const FilePath = `${process.cwd()}/${Filename}`;
+
+        const o = {
+            workerData: {
+                /* Filename, */
+                FilePath
+            }
+        }
+
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(__filename, o);
+
+            worker.once("error", reject);
+    
+            worker.once("message", Result => {
+                worker.unref();
+                resolve(Result);
+            });
+        });
+    }
+
+} else {
+    const FilePath = workerData.FilePath;
+    
+    Read(FilePath).then(data => {
+        parentPort.postMessage(data);
+    });
+}
--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const moment = require('moment');
 const numeral = require('numeral');
-const {readFileSync, existsSync} = require('graceful-fs');
 const SteamID = require('steamid');
 
-const SteamID64Regex = /[0-9]{17}/;
-const TradeOfferURLRegex = /https?:\/\/steamcommunity\.com\/tradeoffer\/new\/\?partner=[0-9]*&token=[a-zA-Z0-9_-]*/;
+const Regex = {
+    SteamID64: /[0-9]{17}/,
+    TradeOfferUrl: /https?:\/\/steamcommunity\.com\/tradeoffer\/new\/\?partner=[0-9]*&token=[a-zA-Z0-9_-]*/,
+    Url: /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+}
 
-module.exports = {
-    "TimeStamp": TimeStamp,
-    "isURL": isURL,
-    "isTradeOfferURL": isTradeOfferURL,
-    "isSteamID64": isSteamID64,
-    "isValidSteamID": isValidSteamID,
-    "GetSteamID64FromURL": GetSteamID64FromURL,
-    "formatNumber": formatNumber,
-    "readJSON": readJSON,
-    "sleep": sleep,
-    "SplitArray": SplitArray
+async function readJSON(Filename = "") {    
+    try {
+        const WorkerFunction = require('./components/WorkerReadJson');
+        const Result = await WorkerFunction(...arguments);
+        return Result;
+    } catch {
+        const Func = require('./components/ReadJson');
+        const Result = await Func(...arguments);
+        return Result;
+    }
 }
 
 function SplitArray(Array = [], MaxSize = 100) {
@@ -39,17 +40,17 @@ function sleep(ms = 1000) {
 }
 
 async function isURL(str = "") {
-    return /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(str);
+    return Regex.Url.test(str);
 }
 
 async function isTradeOfferURL(str = "") {
-    return TradeOfferURLRegex.test(str);
+    return Regex.TradeOfferUrl.test(str);
 }
 
 async function isValidSteamID(value) {
     try {
         return new SteamID(value).isValid();
-    } catch (e) {
+    } catch {
         return false;
     }
 }
@@ -57,17 +58,17 @@ async function isValidSteamID(value) {
 async function isSteamID64(str = "") {
     const isValid = await isValidSteamID(str);
     if (!isValid) return false;
-    return SteamID64Regex.test(str);
+    return Regex.SteamID64.test(str);
 }
 
 async function GetSteamID64FromURL(str = "") {
-    const m = str.match(SteamID64Regex);
+    const m = str.match(Regex.SteamID64);
     if (m) return m[0];
     return m;
 }
 
 async function TimeStamp(date = new Date()) {
-    const ts = date ? moment(date) : moment();
+    const ts = moment(date);
 
     const o = {
         Date: ts.format('YYYY-MM-DD'),
@@ -81,15 +82,15 @@ function formatNumber(number = 1000) {
     return numeral(number).format('0,0');
 }
 
-async function readJSON(Filename = "") {
-    const FilePath = `${process.cwd()}/${Filename}`;
-    if (!await existsSync(FilePath)) return {};
-    const fileData = await readFileSync(FilePath);
-    try {
-        return await JSON.parse(fileData);
-    } catch (e) {
-        //Log.Warn(`Failed to parse "${filename}" to JSON.`);
-        //return Promise.reject(`Failed to parse "${filename}" to JSON.`);
-        return {};
-    }
+module.exports = {
+    TimeStamp,
+    isURL,
+    isTradeOfferURL,
+    isSteamID64,
+    isValidSteamID,
+    GetSteamID64FromURL,
+    formatNumber,
+    readJSON,
+    sleep,
+    SplitArray
 }
